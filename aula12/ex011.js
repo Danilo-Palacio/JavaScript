b28ca71179fa1ee5e9df7847c39f9e96529d664d
Danilo Palacio <114814797+Danilo-Palacio@users.noreply.github.com>
2022-12-08 18:20:18 -0300

    Update ex011.js

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,12 +1,14 @@
-var idade = 67
+var idade = 12
+var agora = new Date()
+var hora = agora.
+
+
 console.log(`Você tem ${idade} anos.`)
 
 if(idade<16){
-    console.log('Não vota')
-}else if (idade < 18 || idade> 65){
+                console.log('Não vota')
+    }else if (idade < 18 || idade> 65){
         console.log('Voto opcional')
-   
-}else {
-    console.log('Voto obrigatório')
-
-}
+    }else {
+        console.log('Voto obrigatório')
+    }
