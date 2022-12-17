72b0b2a9ec6add609b1d82db1f2e6d2b246b536e
Danilo Palacio <114814797+Danilo-Palacio@users.noreply.github.com>
2022-12-17 01:13:13 -0300

    finalizado o curso

--- aula19/script.js
+++ aula19/script.js
@@ -1,6 +1,62 @@
-homem 20 
-homem 44
-fem 44
-mulher 20
-homem 4
-mulher 60
+let num = document.querySelector('#txtnum')
+let sel = document.querySelector('#select')
+let res = document.querySelector('#resultado')
+var adicionado = [];
+
+
+
+function adicionar(){
+
+    if (num.value.length == 0 || num.value > 100){
+        alert('Colocar o numero de 1 a 100')
+    }else{
+        adicionado.push(Number(num.value))
+
+        var option = document.createElement('option')
+        option.text=`Valor ${num.value} adicionado.  `
+        sel.appendChild(option)
+        res.innerHTML = ' '
+    }
+    num.value = ' '
+    num.focus()
+    
+
+}
+
+
+function finalizar(){
+
+    if(adicionado.length==0){
+        alert('Adicione valores antes de finalizar!')
+    }else{
+
+        let total = adicionado.length
+        let maior = adicionado[0]
+        let menor = adicionado[0]
+        let soma = 0;
+
+
+        for ( let pos in adicionado){
+            soma+= adicionado[pos]
+
+            if(adicionado[pos] > maior)
+            maior = adicionado[pos]
+
+            if ( adicionado[pos] < menor)
+            menor = adicionado[pos]
+        }
+
+       
+        res.innerHTML += `Ao todo temos ${total} números cadastrados <br>
+
+        O maior valor informado foi ${maior}. <br>
+        O menor valor informado foi ${menor}.<br>
+
+        Somando todos os valores, temos ${soma}. <br>
+        A média dos valores digitados é ${soma/ adicionado.length}. <br>
+        `
+
+
+    }
+}
+
---
+++ aula19/teste.js
@@ -0,0 +1,9 @@
+var numbers = [1,2,4];
+
+var sum = 0;
+
+for(var i in numbers){
+   sum+=numbers[i];
+}
+
+console.log(sum);
