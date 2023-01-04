6e5279ecf1b08cd27ef210fb4dab780a541d9454
Danilo Palacio <114814797+Danilo-Palacio@users.noreply.github.com>
2023-01-04 00:25:32 -0300

    Create testes.js

---
+++ testes.js
@@ -0,0 +1,25 @@
+
+
+
+let techs = ['html','css','js']
+
+
+
+
+// adicionar no Fim
+techs.push('nodejs')
+//adicionar no começo
+techs.unshift('sql')
+// remover do fim
+techs.pop()
+//Remover do começo
+techs.shift()
+//Pegar somente alguns elementos do array
+//console.log(techs.slice(1,3)//Elemento de inicio e elemento de fim
+//remover 1 ou mais items em qualquer posição do array
+//tech.splice(1,1)
+//encontrar a posição de um elemento no array
+let index = techs.indexOf("css")
+
+console.log(techs)
+console.log(index)
